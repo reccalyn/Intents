af2791584989ba968f855f8306503b9fdc5e8012
reccalyn <sreccalyn@gmail.com>
2020-11-07 01:06:12 +0800

    Create username and password

--- app/src/main/java/com/example/intents/MainActivity.kt
+++ app/src/main/java/com/example/intents/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.intents
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.loginBtn).setOnClickListener {
+            val user = findViewById<EditText>(R.id.usernameTxt)
+            val username = user.text.toString();
+
+            val pass = findViewById<EditText>(R.id.passwordTxt)
+            val password = pass.text.toString();
+
     }
 }
