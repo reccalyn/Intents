94679fc09814b2ce84065511e9a50ed80dbc7357
reccalyn <sreccalyn@gmail.com>
2020-11-07 01:26:48 +0800

    Create buttons function

--- app/src/main/java/com/example/intents/nextActivity.kt
+++ app/src/main/java/com/example/intents/nextActivity.kt
@@ -1,8 +1,12 @@
 package com.example.intents
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock
+import android.provider.Settings
 import android.widget.Button
+import android.widget.Toast
 
 class nextActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,22 +21,42 @@ class nextActivity : AppCompatActivity() {
     }
 
     private fun showCalendar(){
-
+        val intent = Intent(Intent.ACTION_MAIN)
+        intent.addCategory(Intent.CATEGORY_APP_CALENDAR)
+        startActivity(intent)
     }
 
     private fun showWifi(){
-
+        val intent = Intent(Settings.ACTION_WIFI_SETTINGS)
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        }
     }
 
     private fun showMessages(){
-
+        val intent = Intent(Intent.ACTION_MAIN)
+        intent.addCategory(Intent.CATEGORY_APP_MESSAGING)
+        startActivity(intent)
     }
 
     private fun showSettings(){
-
+        val intent = Intent(Settings.ACTION_SETTINGS)
+        intent.addCategory(Intent.ACTION_ALL_APPS) //The intent fail due to . . . .
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        } else {
+            Toast.makeText(this, "Cannot open SETTINGS.", Toast.LENGTH_SHORT).show()
+        }
     }
 
     private fun showAlarm(){
-
+        val intent = Intent(AlarmClock.ACTION_SET_ALARM)
+        intent.addCategory(Intent.ACTION_ALL_APPS)
+        startActivity(intent)
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        } else {
+            Toast.makeText(this, "Cannot open clock.", Toast.LENGTH_SHORT).show()
+        }
     }
 }
