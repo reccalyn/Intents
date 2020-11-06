12714e68070822c83654c884575a88f37440b898
reccalyn <sreccalyn@gmail.com>
2020-11-07 01:22:06 +0800

    Create buttons function

--- app/src/main/java/com/example/intents/nextActivity.kt
+++ app/src/main/java/com/example/intents/nextActivity.kt
@@ -9,18 +9,18 @@ class nextActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_next)
 
-        findViewById<Button>(R.id.calendarBtn).setOnClickListener { showClock() }
-        findViewById<Button>(R.id.wifiBtn).setOnClickListener { showCalendar() }
+        findViewById<Button>(R.id.calendarBtn).setOnClickListener { showCalendar() }
+        findViewById<Button>(R.id.wifiBtn).setOnClickListener { showWifi() }
         findViewById<Button>(R.id.messagesBtn).setOnClickListener { showMessages() }
         findViewById<Button>(R.id.settingsBtn).setOnClickListener { showSettings() }
         findViewById<Button>(R.id.alarmBtn).setOnClickListener { showAlarm() }
     }
 
-    private fun showClock(){
+    private fun showCalendar(){
 
     }
 
-    private fun showCalendar(){
+    private fun showWifi(){
 
     }
 
